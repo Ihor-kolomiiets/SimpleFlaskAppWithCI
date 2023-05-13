399dd193d9e812ef3df0b5dae3067db4927ec4de
Ihor Kolomiiets <Ihor.kolomiiets@gmail.com>
2023-05-13 12:23:40 +0300

    add path to about page

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Hello, World!"})
 
+@app.route("/about")
+def about():
+    return jsonify({"owner": "Ihor Kolomiiets"})
+
 if __name__ == "__main__":
     app.run()
